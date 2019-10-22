f9be2a54e3de532f2346c3b99c91d06d26443ea8
Cameron Moir <cameronjmoir@gmail.com>
2019-10-22 16:38:35 +0100

    connected exchange rates to app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#app",
+    data: {
+      currencies: []
+    },
+    mounted(){
+      this.getCurrencies()
+    },
+    methods: {
+      getCurrencies: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(currencies => this.currencies = currencies)
+      },
+    }
+  })
+
+})
