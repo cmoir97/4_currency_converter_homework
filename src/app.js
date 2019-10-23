36435ae33eac7d1b6b020a0054d9bf6626cfbb56
Cameron Moir <cameronjmoir@gmail.com>
2019-10-23 09:00:12 +0100

    mvp complete

--- src/app.js
+++ src/app.js
@@ -4,7 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
-      currencies: []
+      currencies: [],
+      selectedCurrency: 0,
+      euroAmount: 0
+    },
+    computed: {
+      toEuroConversion: function() {
+        return this.euroAmount * this.selectedCurrency
+      },
+      fromEuroConversion: function() {
+        return this.euroAmount / this.selectedCurrency
+      }
     },
     mounted(){
       this.getCurrencies()
